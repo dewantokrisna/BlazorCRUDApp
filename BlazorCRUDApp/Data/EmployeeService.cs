d9cd05e0013b58dd219c24633fe5f42bf2831045
Dewanto Krisna <dewanto.krisna@gmail.com>
2021-01-25 08:40:01 +0700

    first commit

---
+++ BlazorCRUDApp/Data/EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorCRUDApp.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Employee 1"
+            },
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Employee 2"
+            }
+        };
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
---
+++ BlazorCRUDApp/Data/IEmployeeService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorCRUDApp.Data
+{
+    interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+    }
+}
