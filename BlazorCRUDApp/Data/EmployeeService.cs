35e098b43bd03be2dd2253731dca311cec3e756f
Dewanto Krisna <dewanto.krisna@gmail.com>
2021-01-29 03:16:30 +0700

    CRUD FINISHED

--- BlazorCRUDApp/Data/EmployeeService.cs
+++ BlazorCRUDApp/Data/EmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BlazorCRUDApp.Data
 {
@@ -18,9 +19,36 @@ namespace BlazorCRUDApp.Data
                 Name = "Employee 2"
             }
         };
+
+        public void AddEmployee(Employee employee)
+        {
+            var id = Guid.NewGuid();
+            employee.Id = id;
+            employees.Add(employee);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            var employee = GetEmployee(id);
+            employees.Remove(employee);
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+
         public List<Employee> GetEmployees()
         {
             return employees;
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var getOldEmployee = GetEmployee(employee.Id);
+            getOldEmployee.Name = employee.Name;
+
         }
     }
 }
--- BlazorCRUDApp/Data/IEmployeeService.cs
+++ BlazorCRUDApp/Data/IEmployeeService.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BlazorCRUDApp.Data
 {
     interface IEmployeeService
     {
         List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee employee);
+
+        void AddEmployee(Employee employee);
+
+        void DeleteEmployee(Guid id);
     }
 }
